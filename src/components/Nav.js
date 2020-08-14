27b61a3937d7730132be2781009a1f2d259611e8
ktaylo59 <ktaylo59@gmail.com>
2020-08-14 07:42:23 -0500

    add div side

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navigation extends Component {
     render() {
         return (
-            
+            <div className='side-bar' ></div>
             );
     }
 }
