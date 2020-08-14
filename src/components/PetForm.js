8e73b31c243e282f95e8c63fd980b6445ae3cdbf
ktaylo59 <ktaylo59@gmail.com>
2020-08-14 06:56:05 -0500

    add div

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -2,5 +2,16 @@ import React from 'react';
 
 
 class PetForm extends React.Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
     }
+
+    render() {
+        return (
+            <div className='page' ></div>
+        );
+    }
+}
+
+export default PetForm;
