653f1eb06941d0f4e6b169669829883bd1114016
ktaylo59 <ktaylo59@gmail.com>
2020-08-14 07:10:35 -0500

    add label name

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -11,7 +11,9 @@ class PetForm extends React.Component {
         return (
             <div className='page' >
                 <h1 id='title'> Yay, we love dogs! Give us <br/> the basics about your pup. </h1>
-                <form className='pet-form'  ></form>
+                <form className='pet-form'  >
+                  <label id='name' htmlFor=""> Name </label>
+                </form>
             </div>
         );
     }
