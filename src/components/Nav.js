add9370626a0b9fc6e1e204cc134922008b1da70
ktaylo59 <ktaylo59@gmail.com>
2020-08-14 15:32:25 -0500

    add side buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,14 @@ class Nav extends Component {
     render() {
         return (
             <div className='side-bar' >
+                <img id='pal' src="./assets/goldie.jpg" alt=""/>
                 <h1> PAWTASTIC </h1>
+            <div className= 'form'>
+                   <button> </button>
+                   <button></button>
+                   <button></button>
+                   <button></button>
+            </div>
             </div>
             );
     }
