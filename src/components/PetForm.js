88e5a45c8e9efd476453cefc6c3e918af87e91d7
ktaylo59 <ktaylo59@gmail.com>
2020-08-14 07:31:12 -0500

    add weight

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -22,6 +22,7 @@ class PetForm extends React.Component {
                   <input id='gender-check' type="checkbox" />
                   <label id= 'fixed' htmlFor=""> Sprayed and neutered </label>
                   <input id='fixed-check' type="checkbox"/>
+                  <label id='weight' htmlFor=""> weight </label>
                 </form>
             </div>
         );
